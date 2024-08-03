91140f8cbdcf059905f7745d84f184e74fc7497f
jeongjaeeom-29cm <jjeom@29cm.co.kr>
2024-08-03 13:52:21 +0900

    test: 백준 4949번: 균형잡힌 세상 풀이 단위 테스트 추가

--- learning-algorithms-with-kotlin/src/test/kotlin/io/logantect/algorithms/backjoon/p4949/WorldInBalanceTest.kt
+++ learning-algorithms-with-kotlin/src/test/kotlin/io/logantect/algorithms/backjoon/p4949/WorldInBalanceTest.kt
@@ -1,13 +1,22 @@
 package io.logantect.algorithms.backjoon.p4949
 
-import org.junit.jupiter.api.Assertions.*
-
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class WorldInBalanceTest {
 
-    @Test
-    fun solution() {
-        WorldInBalance.solution()
+    @ParameterizedTest
+    @CsvSource(
+        "So when I die (the [first] I will see in (heaven) is a score list)., true",
+        "[ first in ] ( first out )., true",
+        "Half Moon tonight (At least it is better than no Moon at all]., false",
+        "A rope may form )( a trail in a maze., false",
+        "Help( I[m being held prisoner in a fortune cookie factory)]., false",
+        "([ (([( [ ] ) ( ) (( ))] )) ])., true",
+        "., true"
+    )
+    fun testExampleCases(input: String, expected: Boolean) {
+        assertEquals(expected, WorldInBalance.solution(input))
     }
 }
