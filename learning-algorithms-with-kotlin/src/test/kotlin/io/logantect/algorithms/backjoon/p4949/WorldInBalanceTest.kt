498d3fa41098d8c1b8eb4b04a930833fb33506f3
jeongjaeeom-29cm <jjeom@29cm.co.kr>
2024-08-03 13:48:47 +0900

    feat: 백준 4949번: 균형잡힌 세상 풀이

---
+++ learning-algorithms-with-kotlin/src/test/kotlin/io/logantect/algorithms/backjoon/p4949/WorldInBalanceTest.kt
@@ -0,0 +1,13 @@
+package io.logantect.algorithms.backjoon.p4949
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class WorldInBalanceTest {
+
+    @Test
+    fun solution() {
+        WorldInBalance.solution()
+    }
+}
