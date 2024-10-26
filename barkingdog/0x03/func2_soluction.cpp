ba50ae6c167e7d7d6bf0d01c9621c1bc19a0e198
jeongjaeeom-29cm <jjeom@29cm.co.kr>
2024-10-26 20:08:10 +0900

    feat: func2_soluction.cpp

--- barkingdog/0x03/func2_soluction.cpp
+++ barkingdog/0x03/func2_soluction.cpp
@@ -1,12 +1,23 @@
-int func2(int arr[], int size) {
+#include <bits/stdc++.h>
+using namespace std;
 
+int func2(int arr[], int N) {
+    int occur[101] = {};
+    for (int i = 0; i < N; i++) {
+        if (occur[100 - arr[i]] == 1) {
+            return 1;
+        }
+        occur[arr[i]] = 1;
+    }
+    return 0;
 }
 
 int main(void) {
     int arr1[3] = {1, 52, 48};
     int arr2[2] = {50, 42};
     int arr3[4] = {4, 13, 63, 87};
-    func2(arr1, 3); // 1
-    func2(arr2, 2); // 2
-    func2(arr3, 4); // 3
+
+    cout << func2(arr1, 3); cout << "\n";   // 1
+    cout << func2(arr2, 2); cout << "\n";   // 0
+    cout << func2(arr3, 4);                 // 1
 }
